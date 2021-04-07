91e8b60a1c1d4ea8cc8612742bafda48a602a899
geonwoo <78127896+bokko7711@users.noreply.github.com>
2021-04-08 03:06:36 +0900

    lecture 3,4

--- Tutorial2/src/Main.java
+++ Tutorial2/src/Main.java
@@ -1,22 +1,27 @@
 
 public class Main {
 	
-	final static double PI=3.141592;
-	//final => 상수라는 의미. 절대 바뀔 수 없음.(const같은 애)
-	//static : 이 클래스 내부에서만 공유하는 자원이라는 뜻.
-	final static int INT_MAX=2147483647;
-	//자바에서 int가 담을 수 있는 최대값은 21...저거임. 이 이상의 숫자 대입시 overflow 발생.
-	
 	public static void main(String[] args) {
-		int r=30;
-		System.out.println(r*r*PI);
-		
-		int x=1,y=2;
-		
-		System.out.println("x+y= "+(x+y));
-		System.out.println("x-y= "+(x-y));
-		System.out.println("x*y= "+(x*y));
-		System.out.println("x/y= "+(x/y));
+		//자바 변수는 초기화하지 않으면 사용할 수 없음.
+
+		int x=5;
+
+	    int y=3;
+
+	    System.out.println(max(x,y));
 	}
 
+	  //함수를 다른 곳에서도 호출하려면 static 키워드 필요.
+
+	  static int max(int a,int b){
+
+	  	return a>b? a:b;
+
+	  }
+
+	  //substring
+
+	  //Math.pow()=>거듭제곱
+	
+	
 }
