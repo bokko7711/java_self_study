6f18acf1a955abbe46cb2363df6932b9dbae8cc9
geonwoo <78127896+bokko7711@users.noreply.github.com>
2021-04-27 12:20:30 +0900

    lecture 18 finished

---
+++ Tutorial2/src/Dog.java
@@ -0,0 +1,10 @@
+
+public class Dog extends Animal{
+
+	@Override
+	void bark() {
+		// TODO Auto-generated method stub
+		System.out.println("Пе!Пе!");
+	}
+
+}
--- Tutorial2/src/Main.java
+++ Tutorial2/src/Main.java
@@ -1,15 +1,7 @@
-import java.util.Scanner;
+public class Main{
 
-public class Main {
-	
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		Student std1=new Student("최건우",23,176,71,"19011583",1,4.5);
-		Teacher tchr1=new Teacher("김덕배",53,166,56,"876847",330,25);
-		
-		int classNumber=sc.nextInt();
-		Student classMates[]=new Student[classNumber];//객체 배열 선언 및 동적 할당
-		std1.show();
-		tchr1.show();
+		Dog dog=new Dog();
+		dog.bark();
 	}
 }
